5e565022e1cefcd87a1d91ce945b0f76e0569e58
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2012-03-20 13:49:08 +0100

    filesystem friendly filenames: fix in mockservice and mockservicetest for this.

--- coin-api-war/src/test/java/nl/surfnet/coin/api/service/MockServiceTest.java
+++ coin-api-war/src/test/java/nl/surfnet/coin/api/service/MockServiceTest.java
@@ -35,6 +35,12 @@ public class MockServiceTest {
   }
 
   @Test
+  public void getPersonSpecialChars() {
+    Person person = service.getPerson("spec-ial:cha_rs*in^file.name", "some logged in user").getEntry();
+    assertEquals("myspecialcharsfamilyname", person.getName().getFamilyName());
+  }
+
+  @Test
   public void getGroupMembers() {
     List<Person> groupMembers = service.getGroupMembers("foo",
         "some logged in user").getEntry();
