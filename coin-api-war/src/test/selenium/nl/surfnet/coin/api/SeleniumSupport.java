b5d15f7607ec929b24a4cdef3f762aa6bdb71637
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2012-03-26 13:31:38 +0200

    Changed to HtmlUnitDriver as preferred (should work better on Bamboo)

--- coin-api-war/src/test/selenium/nl/surfnet/coin/api/SeleniumSupport.java
+++ coin-api-war/src/test/selenium/nl/surfnet/coin/api/SeleniumSupport.java
@@ -19,8 +19,11 @@ package nl.surfnet.coin.api;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,7 +35,7 @@ public class SeleniumSupport {
   
   private final static Logger LOG = LoggerFactory.getLogger(SeleniumSupport.class);
 
-  private static FirefoxDriver driver;
+  private static WebDriver driver;
 
   private static String withEndingSlash(String path) {
     return path.endsWith("/") ? path : path + "/";
@@ -41,38 +44,57 @@ public class SeleniumSupport {
   @Before
   public void initializeOnce() {
     if (driver == null) {
-      SeleniumSupport.driver = new FirefoxDriver();
-      SeleniumSupport.driver.manage().timeouts()
-          .implicitlyWait(3, TimeUnit.SECONDS);
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-        @Override
-        public void run() {
-          if (driver != null) {
-            driver.quit();
-          }
-        }
-      });
+      // choose between HtmlUnit and Firefox
+      initHtmlUnitDriver();
     }
+  }
+
+  private void initHtmlUnitDriver() {
+    SeleniumSupport.driver = new HtmlUnitDriver();
+    SeleniumSupport.driver.manage().timeouts()
+        .implicitlyWait(3, TimeUnit.SECONDS);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        if (driver != null) {
+          driver.quit();
+        }
+      }
+    });
+  }
+
+  private void initFirefoxDriver() {
+    SeleniumSupport.driver = new FirefoxDriver();
+    SeleniumSupport.driver.manage().timeouts()
+        .implicitlyWait(3, TimeUnit.SECONDS);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        if (driver != null) {
+          driver.quit();
+        }
+      }
+    });
   }
 
   /**
    * @return the webDriver
    */
-  protected FirefoxDriver getWebDriver() {
+  protected WebDriver getWebDriver() {
     return driver;
   }
 
   protected void loginEndUser() {
     // log in...
-    getWebDriver().findElementByName("j_username").sendKeys("bob");
-    getWebDriver().findElementByName("j_password").sendKeys("bobspassword");
-    getWebDriver().findElementByName("submit").click();
+    getWebDriver().findElement(By.name("j_username")).sendKeys("bob");
+    getWebDriver().findElement(By.name("j_password")).sendKeys("bobspassword");
+    getWebDriver().findElement(By.name("submit")).click();
   }
 
   protected void giveUserConsentIfNeeded() {
     WebElement authorizeButton = null;
     try {
-      authorizeButton = getWebDriver().findElementByName("authorize");
+      authorizeButton = getWebDriver().findElement(By.name("authorize"));
     } catch (RuntimeException e) {
 
     }
