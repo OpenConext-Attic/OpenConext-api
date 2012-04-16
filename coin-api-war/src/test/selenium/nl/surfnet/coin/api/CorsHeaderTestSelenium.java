b14a9631773836e3bcb88b3cfea2667d8f405a3c
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2012-04-16 10:38:40 +0200

    BACKLOG-373: Implemented proper test for CORS headers.

--- coin-api-war/src/test/selenium/nl/surfnet/coin/api/CorsHeaderTestSelenium.java
+++ coin-api-war/src/test/selenium/nl/surfnet/coin/api/CorsHeaderTestSelenium.java
@@ -18,14 +18,51 @@ package nl.surfnet.coin.api;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.SignatureType;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.surfnet.coin.api.client.OpenConextApi10aTwoLegged;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-public class CorsHeaderTestSelenium extends SeleniumSupport {
+public class CorsHeaderTestSelenium {
+
+  private static final String OAUTH_KEY = "https://testsp.test.surfconext.nl/shibboleth";
+  private static final String OAUTH_SECRET = "mysecret";
+
+  private static final String USER_ID = "urn:collab:person:test.surfguest.nl:oharsta";
+  private static final String OS_URL = "social/rest/people/" + USER_ID + "/@self";
+  private final static String OAUTH_OPENCONEXT_API_READ_SCOPE = "read";
+
+  private String getApiBaseUrl() {
+    return System.getProperty("selenium.test.url", "http://localhost:8095/");
+  }
 
   @Test
   public void corsHeader() throws Exception {
-  // TODO: test for existence of Cors-header. But how? WebDriver does not expose http headers.
-    // Should be an integration test then, using HttpClient and client-credentials profile?
+    OAuthService service = new ServiceBuilder()
+        .provider(new OpenConextApi10aTwoLegged())
+        .apiKey(OAUTH_KEY)
+        .apiSecret(OAUTH_SECRET)
+        .scope(OAUTH_OPENCONEXT_API_READ_SCOPE)
+        .callback("oob")
+        .signatureType(SignatureType.QueryString)
+        .debug()
+        .build();
+
+    OAuthRequest req = new OAuthRequest(Verb.GET, getApiBaseUrl() + OS_URL);
+    service.signRequest(new Token("", ""), req);
+    Response response = req.send();
+    assertNotNull("Response should contain CORS-header", response.getHeaders().get("Access-Control-Allow-Origin"));
+    assertTrue("CORS-header should be '*'", response.getHeaders().get("Access-Control-Allow-Origin").equals("*"));
   }
 }
