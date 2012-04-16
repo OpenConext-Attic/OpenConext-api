12d6930551ba5ae65dc01ad00a37c2227468d12f
Jasha Joachimsthal <jasha.joachimsthal@flex.surfnet.nl>
2012-04-16 11:17:49 +0200

    IDE was complaining about missing spring-context dependency

--- coin-api-external-groups/src/main/java/nl/surfnet/coin/teams/service/impl/GroupServiceThreeLeggedOAuth10a.java
+++ coin-api-external-groups/src/main/java/nl/surfnet/coin/teams/service/impl/GroupServiceThreeLeggedOAuth10a.java
@@ -17,42 +17,38 @@
 package nl.surfnet.coin.teams.service.impl;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
 import org.scribe.model.Token;
 import org.scribe.oauth.OAuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import nl.surfnet.coin.api.client.OpenConextJsonParser;
 import nl.surfnet.coin.api.client.domain.Group20;
-import nl.surfnet.coin.api.client.domain.Group20Entry;
 import nl.surfnet.coin.api.client.domain.GroupMembersEntry;
 import nl.surfnet.coin.api.client.domain.Person;
 import nl.surfnet.coin.teams.domain.GroupProvider;
 import nl.surfnet.coin.teams.domain.GroupProviderUserOauth;
 import nl.surfnet.coin.teams.domain.ThreeLeggedOauth10aGroupProviderApi;
-import nl.surfnet.coin.teams.service.GroupProviderService;
 import nl.surfnet.coin.teams.service.GroupService;
 import nl.surfnet.coin.teams.util.GroupProviderOptionParameters;
 
 import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.PROPERTY_DESCRIPTION;
 import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.PROPERTY_NAME;
 import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.convertProperty;
+import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.convertToExternalGroupId;
 import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.convertToExternalPersonId;
-import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.*;
+import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.convertToSurfConextGroupId;
+import static nl.surfnet.coin.teams.util.GroupProviderPropertyConverter.convertToSurfConextPersonId;
 
 /**
  * Implementation for a {@link GroupService} using 3-legged OAuth
