67f1a669b9517c8939647980e98a5b982ef31b81
oharsta <oharsta@zilverline.com>
2012-04-19 23:01:31 +0200

    Fixed problem http://stackoverflow.com/questions/3526523/spring-mvc-pathvariable-getting-truncated

--- coin-api-war/src/main/java/nl/surfnet/coin/api/MockApiController.java
+++ coin-api-war/src/main/java/nl/surfnet/coin/api/MockApiController.java
@@ -49,34 +49,33 @@ public class MockApiController extends AbstractApiController {
 
   @Value("${mock-api-enabled}")
   private boolean mockApiEnabled;
-  
+
   @Resource(name = "mockService")
   private PersonService personService;
 
   @Resource(name = "mockService")
   private GroupService groupService;
 
-  @RequestMapping(value = "/people/{userId}/@self")
+  @RequestMapping(value = "/people/{userId:.+}/@self")
   @ResponseBody
   public PersonEntry getPersonAtSelf(@PathVariable("userId")
   String userId) {
     return getPerson(userId);
   }
 
-  @RequestMapping(value = "/people/{userId}")
+  @RequestMapping(value = "/people/{userId:.+}")
   @ResponseBody
   public PersonEntry getPerson(@PathVariable("userId")
   String userId) {
     invariant();
-    LOG.info("Got getPerson-request, for userId '{}' on behalf of '{}'",
-        new Object[] { userId, getOnBehalfOf() });
+    LOG.info("Got getPerson-request, for userId '{}' on behalf of '{}'", new Object[] { userId, getOnBehalfOf() });
     if (PersonController.PERSON_ID_SELF.equals(userId)) {
       userId = getOnBehalfOf();
     }
     return personService.getPerson(userId, getOnBehalfOf());
   }
 
-  @RequestMapping(value = "/people/{userId}/{groupId}")
+  @RequestMapping(value = "/people/{userId:.+}/{groupId:.+}")
   @ResponseBody
   public GroupMembersEntry getGroupMembers(@PathVariable("userId")
   String userId, @PathVariable("groupId")
@@ -97,7 +96,7 @@ public class MockApiController extends AbstractApiController {
     return groupMembers;
   }
 
-  @RequestMapping(value = "/groups/{userId}")
+  @RequestMapping(value = "/groups/{userId:.+}")
   @ResponseBody
   public Group20Entry getGroups(@PathVariable("userId")
   String userId, @RequestParam(value = "count", required = false)
@@ -107,9 +106,8 @@ public class MockApiController extends AbstractApiController {
     invariant();
     if (PersonController.PERSON_ID_SELF.equals(userId)) {
       userId = getOnBehalfOf();
-}
-    LOG.info("Got getGroups-request, for userId '{}',  on behalf of '{}'",
-        new Object[] { userId, getOnBehalfOf() });
+    }
+    LOG.info("Got getGroups-request, for userId '{}',  on behalf of '{}'", new Object[] { userId, getOnBehalfOf() });
     Group20Entry groups = groupService.getGroups20(userId, getOnBehalfOf());
     List<Group20> entry = groups.getEntry();
     entry = (List<Group20>) processQueryOptions(groups, count, startIndex, sortBy, entry);
@@ -120,8 +118,19 @@ public class MockApiController extends AbstractApiController {
     if (!this.mockApiEnabled) {
       throw new RuntimeException("Mock API not enabled");
     }
-    
+
   }
 
-  
+  public void setPersonService(PersonService personService) {
+    this.personService = personService;
+  }
+
+  public void setGroupService(GroupService groupService) {
+    this.groupService = groupService;
+  }
+
+  public void setMockApiEnabled(boolean mockApiEnabled) {
+    this.mockApiEnabled = mockApiEnabled;
+  }
+
 }
