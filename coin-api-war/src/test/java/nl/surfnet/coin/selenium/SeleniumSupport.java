f058f3ee0ba5df2d79615fd04b76cfacb9d5e0a7
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2012-11-01 10:59:50 +0100

    added screenshot capability

--- coin-api-war/src/test/java/nl/surfnet/coin/selenium/SeleniumSupport.java
+++ coin-api-war/src/test/java/nl/surfnet/coin/selenium/SeleniumSupport.java
@@ -16,19 +16,29 @@
 
 package nl.surfnet.coin.selenium;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.fail;
 
 /**
  *
@@ -45,6 +55,10 @@ public class SeleniumSupport {
   protected String getApiBaseUrl() {
     return System.getProperty("selenium.test.url", "http://localhost:8095/api/");
   }
+
+  @Rule
+  public ScreenshotTestRule screenshotTestRule = new ScreenshotTestRule();
+
 
   @Before
   public void initializeOnce() {
@@ -114,6 +128,7 @@ public class SeleniumSupport {
     getWebDriver().findElement(By.name("j_username")).sendKeys("user");
     getWebDriver().findElement(By.name("j_password")).sendKeys("secret");
     getWebDriver().findElement(By.name("login")).submit();
+
   }
 
   protected void giveUserConsentIfNeeded() {
@@ -127,6 +142,38 @@ public class SeleniumSupport {
     if (authorizeButton != null) {
       LOG.debug("Clicking 'authorize'-button on user consent form");
       authorizeButton.click();
+    }
+  }
+
+  public static class ScreenshotTestRule implements MethodRule {
+    public Statement apply(final Statement statement, final FrameworkMethod frameworkMethod, final Object o) {
+      return new Statement() {
+        @Override
+        public void evaluate() throws Throwable {
+          try {
+            statement.evaluate();
+          } catch (Throwable t) {
+            // exception will be thrown only when a test fails.
+            captureScreenshot(frameworkMethod);
+            // rethrow to allow the failure to be reported by JUnit
+            throw t;
+          }
+        }
+
+        public void captureScreenshot(FrameworkMethod method) {
+          try {
+            File screenshot =  ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String outputFile = String.format("%s%s%d.png", System.getProperty("java.io.tmpdir"), System.getProperty("file.separator"), System.currentTimeMillis());
+
+            LOG.info("Screenshot for failed method {}.{} will be saved to: {}", new Object[] {
+              method.getClass(), method.getName(), outputFile});
+            FileUtils.copyFile(screenshot, new File(outputFile));
+          } catch (Exception e) {
+            // No need to crash the tests if the screenshot fails
+            LOG.debug("Saving a screenshot failed: {}", e.getMessage());
+          }
+        }
+      };
     }
   }
 
