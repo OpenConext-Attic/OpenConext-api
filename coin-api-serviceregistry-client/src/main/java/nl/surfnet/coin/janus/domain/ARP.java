009e68d604bd467db3a9fe38c45505ef39bf7d2b
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2013-05-30 15:35:30 +0200

    Improved ARP representation to explicitly differentiate between 'no arp', 'no attributes' and 'some attributes'

--- coin-api-serviceregistry-client/src/main/java/nl/surfnet/coin/janus/domain/ARP.java
+++ coin-api-serviceregistry-client/src/main/java/nl/surfnet/coin/janus/domain/ARP.java
@@ -16,6 +16,7 @@
 
 package nl.surfnet.coin.janus.domain;
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,11 +24,14 @@ import java.util.Map;
 /**
  * Attribute Release Policy
  */
-public class ARP {
+public class ARP implements Serializable {
 
+  private static final long serialVersionUID = 0L;
   private String name;
   private String description;
   private Map<String, List<Object>> attributes = new LinkedHashMap<String, List<Object>>();
+  private boolean noArp;
+  private boolean noAttrArp;
 
   public String getName() {
     return name;
@@ -55,11 +59,23 @@ public class ARP {
 
   public static ARP fromRestResponse(Map response) {
     ARP arp = new ARP();
+
+    arp.setNoArp(false);
+    arp.setNoAttrArp(false);
+
+    if (response.isEmpty()) {
+      arp.setNoArp(true);
+      return arp;
+    }
+
     arp.setName((String) response.get("name"));
     arp.setDescription((String) response.get("description"));
     final Object attr = response.get("attributes");
     if (attr instanceof Map) {
       arp.setAttributes((Map<String, List<Object>>) attr);
+    } else {
+      // If 'no attributes', Janus will return not a hash, but an empty array
+      arp.setNoAttrArp(true);
     }
     return arp;
   }
@@ -73,5 +89,21 @@ public class ARP {
     sb.append(", attributes=").append(attributes);
     sb.append('}');
     return sb.toString();
+  }
+
+  public boolean isNoArp() {
+    return noArp;
+  }
+
+  public boolean isNoAttrArp() {
+    return noAttrArp;
+  }
+
+  public void setNoArp(boolean noArp) {
+    this.noArp = noArp;
+  }
+
+  public void setNoAttrArp(boolean noAttrArp) {
+    this.noAttrArp = noAttrArp;
   }
 }
